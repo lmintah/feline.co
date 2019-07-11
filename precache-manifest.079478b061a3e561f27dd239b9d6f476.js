aafc8613204c76645c324ea038cf0469d3bdc4d4
Laurence <laurencemintah@gmail.com>
2019-07-11 12:20:28 -0400

    Updates

---
+++ precache-manifest.079478b061a3e561f27dd239b9d6f476.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026c2d9c03ac0b435e05b7a82be1d67d",
+    "url": "/feline.co/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/feline.co/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "c91ca727226e2246f61b",
+    "url": "/feline.co/static/css/main.e6cdbbf2.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/feline.co/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "c91ca727226e2246f61b",
+    "url": "/feline.co/static/js/main.f27cc3fb.chunk.js"
+  },
+  {
+    "revision": "56259ed53b58aa605368",
+    "url": "/feline.co/static/js/runtime~main.90a8c801.js"
+  },
+  {
+    "revision": "bcfa3a4184aa5b37b2a4033f6ec15325",
+    "url": "/feline.co/static/media/Team 401.bcfa3a41.woff"
+  }
+]);
